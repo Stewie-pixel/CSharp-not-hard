b3ad9e23eb29ba5a3d1c9542893c7b2f53042a4c
Stewie-pixel <hanchivi39@gmail.com>
2026-03-20 00:14:44 +1100

    refactor(1.2P): sort codes into repetition resource

---
+++ repetition/task/Snippet7.cs
@@ -0,0 +1,10 @@
+using System;
+class Snippet7 {
+    static void Main() {
+        for(int i = 1; i < 10; i++) {
+            if (i > 2) {
+                Console.WriteLine("Flower");
+            }
+        }
+    }
+}
--- task/Snippet6.cs
+++
@@ -1,8 +0,0 @@
-using System;
-class Snippet6 {
-    static void Main() {
-        for(int count = 1; count < 10; count++) {
-            Console.WriteLine("Hello");
-        }
-    }
-}
